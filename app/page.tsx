8ca93e4240cdb95b74be2303d91017f94ffa2e86
Acil <ec23108@qmul.ac.uk>
2025-01-20 18:38:01 +0000

    Home page updated

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Chatbox from "./components/Chatbox";
 import SuggestionRow from "./components/SuggestionRow";
 export default function Home() {
   return (
-    <div className="flex flex-row justify-center items-center h-screen">
+    <div className="flex flex-row justify-center items-center h-screen bg-[#212121]">
       <div className="flex flex-col gap-5 w-2/3 justify-center">
-        <h1 className="text-4xl font-bold mx-auto">
-          What Can I Help You With?
+        <h1 className="md:text-4xl font-bold mx-auto text-[1.875rem]">
+          What Can I Help With?
         </h1>
         <Chatbox />
         <br />
